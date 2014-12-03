6a9ca6a1c8541e9bb02362fa8f9f6b6cd4549801
Justin Kim <yulli@yulli.org>
2014-12-03 11:54:42 -0500

    Alert the user when they approach the duck

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var duck = document.getElementById("duck");
+
 var distance = function(x1, y1, x2, y2) {
         return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
 };
@@ -10,10 +12,16 @@ var distanceFromMouse = function(element, mouseX, mouseY) {
 };
 
 var monitorMouse = function(e) {
-        var duck = document.getElementById("duck");
-        console.log(distanceFromMouse(duck, e.pageX, e.pageY));
+        if (distanceFromMouse(duck, e.pageX, e.pageY) < 40) {
+                revealDuck();
+                alert("you got too close to the duck");
+        }
 };
 
+var revealDuck = function() {
+        duck.style.visibility = "visible";
+}
+
 window.onload = function() {
-        this.addEventListener('mousemove', monitorMouse);
+        this.addEventListener("mousemove", monitorMouse);
 };
